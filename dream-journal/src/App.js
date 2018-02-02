fd9f4153e67e26dbe5f4d07617b12f659b63cf4d
Anna J <ajeszeck@gmail.com>
2018-02-01 20:02:55 -0800

    Add front end react skeleton.

---
+++ dream-journal/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Your Dream Journal</h1>
+        </header>
+        <p className="App-intro">
+        This is where you can record your dreams.
+        </p>
+        <DreamsContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
