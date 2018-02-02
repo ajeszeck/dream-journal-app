cf06fa022aa07b3451f779c42c791ef345b61a92
Anna J <ajeszeck@gmail.com>
2018-02-02 12:42:58 -0800

    Add tests for users and dreams models.

--- dream-journal-api/spec/models/dream_spec.rb
+++ dream-journal-api/spec/models/dream_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
+require 'shoulda-matchers'
 
 RSpec.describe Dream, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Attributes" do
+    let(:user) { User.create(first_name: "First", last_name: "Last", username: "firstlast", password: "password") }
+    let(:dream) { Dream.create(content: "Test", sleep_date: Date.current, user_id: user.id) }
+
+    it "is valid with content" do
+      expect(dream.valid?).to eq true
+    end
+
+    it "is invalid without content" do
+      dream.content = nil
+      expect(dream.valid?).to eq false
+    end
+
+    it "is valid with a date" do
+      expect(dream.valid?).to eq true
+    end
+
+    it "is invalid without a date" do
+      dream.sleep_date = nil
+      expect(dream.valid?).to eq false
+    end
+  end
+
+  describe "Associations" do
+    it { should belong_to(:user) }
+  end
 end
--- dream-journal-api/spec/models/user_spec.rb
+++ dream-journal-api/spec/models/user_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    let(:user) { User.create(first_name: "First", last_name: "Last", username: "first.last", password: "password") }
+    let(:dream) { Dream.create(content: "Test", sleep_date: Date.current, user_id: user.id) }
+
+    it "is valid with a first_name" do
+      expect(user.valid?).to eq true
+    end
+
+    it "is invalid without a first_name" do
+      user.first_name = nil
+      expect(user.valid?).to eq false
+    end
+
+    it "is valid with a last_name" do
+      expect(user.valid?).to eq true
+    end
+
+    it "is invalid without a last_name" do
+      user.last_name = nil
+      expect(user.valid?).to eq false
+    end
+
+    it "is valid with a username" do
+      expect(user.valid?).to eq true
+    end
+
+    it "is invalid without a username" do
+      user.username = nil
+      expect(user.valid?).to eq false
+    end
+
+    it "is valid with a password" do
+      expect(user.valid?).to eq true
+    end
+
+    it "is invalid without a password" do
+      user.password = nil
+      expect(user.valid?).to eq false
+    end
+  end
+
+  describe "associations" do
+    let(:user) { User.create(first_name: "First", last_name: "Last", username: "first.last", password: "password") }
+    let(:dream_one) { Dream.create(content: "Test_one", sleep_date: Date.current, user_id: user.id) }
+    let(:dream_two) { Dream.create(content: "Test_two", sleep_date: Date.current, user_id: user.id) }
+
+    it { should have_many(:dreams) }
+  end
 end
